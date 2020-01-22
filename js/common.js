c8d93fd11414d52ea7987db4382460794db176ee
GloryOfTheCPSU <mailforgitforproject@gmail.com>
2020-01-22 16:03:27 +0300

    preloader

--- js/common.js
+++ js/common.js
@@ -1,4 +1,13 @@
-const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';//увеличенное изображение
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+}
+
+
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'; //увеличенное изображение
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 const HIDDEN_DETAIL_CLASS = 'hidden-detail';
@@ -13,48 +22,55 @@ function setDetails(imageUrl, titleText) {
   detailImage.setAttribute('src', imageUrl);
   detailTitle.textContent = titleText;
 }
-function imageFromThumb(thumbnail){
+
+function imageFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-url');
 }
-function titleFromThumb(thumbnail){
+
+function titleFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-title');
 }
+
 function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
-function addThumbClickHandler(thumb){
-  thumb.addEventListener('click', function(event){
+
+function addThumbClickHandler(thumb) {
+  thumb.addEventListener('click', function (event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
-    showDetails();``
+    showDetails();
+    ``
   })
 }
-function getThumbnailsArray(){
+
+function getThumbnailsArray() {
   let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   let thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
 
-function hideDetails(){
+function hideDetails() {
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
-function showDetails(){
+
+function showDetails() {
   let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
   frame.classList.add(TINY_EFFECT_CLASS);
-  setTimeout(function(){
+  setTimeout(function () {
     frame.classList.remove(TINY_EFFECT_CLASS);
   }, 100);
-  
+
 }
 
-function addKeyPressHandler(){
-    document.body.addEventListener('keyup', function(event){
+function addKeyPressHandler() {
+  document.body.addEventListener('keyup', function (event) {
     event.preventDefault();
     console.log(event.keyCode);
-      if(event.keyCode === ESC_KEY){
-        hideDetails();
-      }
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
   });
 }
 
@@ -64,3 +80,5 @@ function initializeEvents() {
   addKeyPressHandler();
 }
 initializeEvents();
+
+
