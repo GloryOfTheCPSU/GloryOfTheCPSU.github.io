ff9fce842b5fbe9ac0fbb35765cff1c92caeaac2
GloryOfTheCPSU <mailforgitforproject@gmail.com>
2020-01-22 18:24:17 +0300

    down-up

--- js/common.js
+++ js/common.js
@@ -5,8 +5,51 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 }
+var upDownBtn = document.querySelector('.up_down_btn');
+var check;
+function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+    if (scrolled > coords) {
+      upDownBtn.classList.add('up_down_btn-show');
+      upDownBtn.innerHTML = '&#9650;';
+      upDownBtn.setAttribute('title', 'up');
+      check = false;
+    }
+    if (scrolled === 0) {
+      upDownBtn.innerHTML = '&#9660;';
+      upDownBtn.setAttribute('title', 'down');
+      check = true;
+    }
+  }
+function backToTop() {
+    upDownBtn.classList.add('up_down_btn-disabled');
+    if (!check) {
+      (function goTop() {
+        if (window.pageYOffset !== 0) {
+          window.scrollBy(0, -80);
+          setTimeout(goTop, 0);
+        } else {
+          upDownBtn.classList.remove('up_down_btn-disabled');
+        }
+      })();
+      return;
+    } else if (check) {
+      (function goBottom() {
+        var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
 
-
+        if (match != document.documentElement.scrollHeight) {
+          window.scrollBy(0, 80);
+          setTimeout(goBottom, 0);
+        } else {
+          upDownBtn.classList.remove('up_down_btn-disabled');
+        }
+      })();
+      return;
+    }
+  }
+window.addEventListener('scroll', trackScroll);
+upDownBtn.addEventListener('click', backToTop);
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'; //увеличенное изображение
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
@@ -15,26 +58,21 @@ const ESC_KEY = 27;
 const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const TINY_EFFECT_CLASS = 'is-tiny';
 let firstThumbnail = document.querySelector(THUMBNAIL_LINK_SELECTOR);
-
 function setDetails(imageUrl, titleText) {
   let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
   detailImage.setAttribute('src', imageUrl);
   detailTitle.textContent = titleText;
 }
-
 function imageFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-url');
 }
-
 function titleFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-title');
 }
-
 function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
-
 function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function (event) {
     event.preventDefault();
@@ -43,17 +81,14 @@ function addThumbClickHandler(thumb) {
     ``
   })
 }
-
 function getThumbnailsArray() {
   let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   let thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
-
 function hideDetails() {
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
-
 function showDetails() {
   let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
@@ -63,7 +98,6 @@ function showDetails() {
   }, 100);
 
 }
-
 function addKeyPressHandler() {
   document.body.addEventListener('keyup', function (event) {
     event.preventDefault();
@@ -73,7 +107,6 @@ function addKeyPressHandler() {
     }
   });
 }
-
 function initializeEvents() {
   let thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
